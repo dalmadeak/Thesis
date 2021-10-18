8282fab8989e3e0388a7a350e3ea423932753474
Dalma <cynder097@gmail.com>
2021-10-18 12:30:15 +0200

    Added Mongodb, updated server.js

---
+++ thesis-client/backend/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const Post = require('./models/post');
+
+mongoose.connect("mongodb+srv://elnok:xtiV4hKL05OqaLbM@cluster0.pz2bf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database!')
+  })
+  .catch(() => {
+    console.log('Connection failed.');
+  });
+
+app.use(express.json());
+
+//Ez itt azért kell, mert CORS error-t kapunk, ha a kliens és a szerver különböző host-on fut
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATH, DELETE, OPTIONS');
+  next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  // console.log(post);
+  post.save();
+  res.status(201).json({
+    message: 'Post added successfully'
+  })
+})
+
+app.get('/api/posts',(req, res, next) => {
+  /*const posts = [
+    {
+      id: '00001',
+      title: 'First post',
+      content: 'First post\'s content from the server'
+    },
+    {
+      id: '00002',
+      title: 'Second post',
+      content: 'Second post\'s content from the server'
+    }
+  ];*/
+  Post.find() // returns all entries
+    .then(documents => {
+      console.log(documents);
+      res.status(200).json({
+        message: 'Posts fetched succesfully',
+        posts: documents
+      });
+    });
+});
+
+module.exports = app;
--- thesis-client/server.js
+++ thesis-client/server.js
@@ -2,10 +2,55 @@ const http = require('http');
 const app = require('./backend/app');
 const debug = require('debug')('node-angular');
 
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
   res.end('This is my response');
 });
 
 const port = (process.env.PORT || '3000');
 
+server.listen(port); */
+
+const normalizedPort = val => {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port))
+    return val;
+
+  if (port >= 0)
+    return port;
+
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== 'listen')
+    throw error;
+
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated priviledges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  debug('Listening on ' + bind);
+};
+
+const port = normalizedPort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.on('error', onError);
+server.on('listening', onListening);
 server.listen(port);
