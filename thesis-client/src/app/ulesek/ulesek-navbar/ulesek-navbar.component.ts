0ef69aab31a67613f40b3a2324a5bfb295de674a
Dalma <cynder097@gmail.com>
2021-10-24 15:46:27 +0200

    Navbar finished

---
+++ thesis-client/src/app/ulesek/ulesek-navbar/ulesek-navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-ulesek-navbar',
+  templateUrl: './ulesek-navbar.component.html',
+  styleUrls: ['./ulesek-navbar.component.css']
+})
+export class UlesekNavbarComponent {
+}
---
+++ thesis-client/src/app/ulesek/ulesek.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-ulesek',
+  templateUrl: './ulesek.component.html',
+  styleUrls: ['./ulesek.component.css']
+})
+export class UlesekComponent {
+
+}
