db9170617d3867af93c32cbec21ee16caa12ddee
Dalma <cynder097@gmail.com>
2021-10-30 16:50:07 +0200

    Done

--- thesis-client/src/app/atlathatosag/atlathatosag-jegyzokonyvek/atlathatosag-jegyzokonyvek-files/atlathatosag-jegyzokonyvek-files.component.ts
+++ thesis-client/src/app/atlathatosag/atlathatosag-jegyzokonyvek/atlathatosag-jegyzokonyvek-files/atlathatosag-jegyzokonyvek-files.component.ts
@@ -14,14 +14,14 @@ export class AtlathatosagJegyzokonyvekFilesComponent {
 
   jegyzokonyvekObject = [
   {
-    name: 'asd.pdf',
+    name: 'asd2.pdf',
     meetingDate: '2021. október 10.',
     uploadDate: '2021. október 15.',
     committee: 'kgy',
     path: '../../../../assets/images/header-background.png'
   },
   {
-    name: 'asd2.pdf',
+    name: 'asd1.pdf',
     meetingDate: '2021. október 12.',
     uploadDate: '2021. október 15.',
     committee: 'elnokseg',
@@ -35,17 +35,29 @@ export class AtlathatosagJegyzokonyvekFilesComponent {
     path: '../../../../assets/images/header-background.png'
   },
   {
-    name: 'asd4.pdf',
+    name: 'asd5.pdf',
     meetingDate: '2021. október 20.',
     uploadDate: '2021. október 22.',
     committee: 'elnokseg',
     path: '../../../../assets/images/header-background.png'
   },
   {
-    name: 'asd5.pdf',
+    name: 'asd4.pdf',
     meetingDate: '2021. október 21.',
     uploadDate: '2021. október 27.',
     committee: 'tb',
     path: '../../../../assets/images/header-background.png'
   }];
+
+  onSortByName(){
+    return this.jegyzokonyvekObject.sort((a,b) => (a.name > b.name) ? 1 : -1);
+  }
+
+  onSortByMeetingDate(){
+    return this.jegyzokonyvekObject.sort((a,b) => (a.meetingDate > b.meetingDate) ? 1 : -1);
+  }
+
+  onSortByUploadDate(){
+    return this.jegyzokonyvekObject.sort((a,b) => (a.uploadDate > b.uploadDate) ? 1 : -1);
+  }
 }
