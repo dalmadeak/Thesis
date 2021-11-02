04f75bb5e8d1249cf4640299e95d7349d891c7d3
Dalma <cynder097@gmail.com>
2021-11-02 10:49:46 +0100

    Created szolgáltatások components

---
+++ thesis-client/src/app/szolgaltatasok/szolgaltatasok-etkezok/szolgaltatasok-etkezok.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-szolgaltatasok-etkezok',
+  templateUrl: './szolgaltatasok-etkezok.component.html',
+  styleUrls: ['./szolgaltatasok-etkezok.component.css']
+})
+export class SzolgaltatasokEtkezokComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ thesis-client/src/app/szolgaltatasok/szolgaltatasok.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-szolgaltatasok',
+  templateUrl: './szolgaltatasok.component.html',
+  styleUrls: ['./szolgaltatasok.component.css']
+})
+export class SzolgaltatasokComponent {
+
+}
