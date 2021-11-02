2bbb871fd34e6cde3fe0df07d8fe11830e4bbb18
Dalma <cynder097@gmail.com>
2021-11-02 20:30:38 +0100

    Added css and html

--- thesis-client/src/app/szolgaltatasok/szolgaltatasok-belepokartya/szolgaltatasok-belepokartya.component.ts
+++ thesis-client/src/app/szolgaltatasok/szolgaltatasok-belepokartya/szolgaltatasok-belepokartya.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-szolgaltatasok-belepokartya',
   templateUrl: './szolgaltatasok-belepokartya.component.html',
   styleUrls: ['./szolgaltatasok-belepokartya.component.css']
 })
-export class SzolgaltatasokBelepokartyaComponent implements OnInit {
+export class SzolgaltatasokBelepokartyaComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit() {
+    console.log('asd');
   }
-
 }
