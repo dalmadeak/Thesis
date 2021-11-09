e6938cb20a7643eb44d19379a7de957e366b6811
Dalma <cynder097@gmail.com>
2021-11-09 13:33:07 +0100

    Started

--- thesis-client/src/app/sidenav/sorompo-admin/sorompo-admin.component.ts
+++ thesis-client/src/app/sidenav/sorompo-admin/sorompo-admin.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { faDownload, faFile, faFilePdf, faFileArchive, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sorompo-admin',
   templateUrl: './sorompo-admin.component.html',
   styleUrls: ['./sorompo-admin.component.css']
 })
-export class SorompoAdminComponent implements OnInit {
+export class SorompoAdminComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  sorompoObject = [
+  {
+    name: 'Nagy Péter',
+    neptun: 'ABC123',
+    plate: 'CBA321',
+    type: 'Renault Twingo',
+    card: '11111111',
+    mobile: '+36301234567',
+    email: 'peter@gmail.com',
+    registerDate: '2021.11.04.',
+    semester: '2021-2022-1'
+  },
+  {
+    name: 'Nagy Péter',
+    neptun: 'ABC123',
+    plate: 'CBA321',
+    type: 'Renault Twingo',
+    card: '11111111',
+    mobile: '+36301234567',
+    email: 'peter@gmail.com',
+    registerDate: '2021.11.04.',
+    semester: '2021-2022-1'
+  },
+  {
+    name: 'Nagy Péter',
+    neptun: 'ABC123',
+    plate: 'CBA321',
+    type: 'Renault Twingo',
+    card: '11111111',
+    mobile: '+36301234567',
+    email: 'peter@gmail.com',
+    registerDate: '2021.11.04.',
+    semester: '2021-2022-1'
+  },
+  {
+    name: 'Nagy Péter',
+    neptun: 'ABC123',
+    plate: 'CBA321',
+    type: 'Renault Twingo',
+    card: '11111111',
+    mobile: '+36301234567',
+    email: 'peter@gmail.com',
+    registerDate: '2021.11.04.',
+    semester: '2021-2022-1'
+  },
+  {
+    name: 'Nagy Péter',
+    neptun: 'ABC123',
+    plate: 'CBA321',
+    type: 'Renault Twingo',
+    card: '11111111',
+    mobile: '+36301234567',
+    email: 'peter@gmail.com',
+    registerDate: '2021.11.04.',
+    semester: '2021-2022-1'
+  },
+  {
+    name: 'Abdulmuttalib Abdul Kherim',
+    neptun: 'ABC123',
+    plate: 'CBA321',
+    type: 'Renault Twingo',
+    card: '11111111',
+    mobile: '+36301234567',
+    email: 'abdulkherim1998@gmail.com',
+    registerDate: '2021.11.04.',
+    semester: '2021-2022-1'
+  }];
 
 }
