7b175455b5bd91dbaf3d194d9e7ef49a8b6ed053
Dalma <cynder097@gmail.com>
2021-11-09 17:06:10 +0100

    Added unfinished sorompo admin

--- thesis-client/src/app/sidenav/sorompo-admin/sorompo-admin.component.ts
+++ thesis-client/src/app/sidenav/sorompo-admin/sorompo-admin.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { faDownload, faFile, faFilePdf, faFileArchive, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faComment } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sorompo-admin',
@@ -7,6 +7,8 @@ import { faDownload, faFile, faFilePdf, faFileArchive, faChevronDown } from '@fo
   styleUrls: ['./sorompo-admin.component.css']
 })
 export class SorompoAdminComponent{
+  faComment = faComment;
+  faCheck = faCheck;
 
   sorompoObject = [
   {
@@ -18,7 +20,9 @@ export class SorompoAdminComponent{
     mobile: '+36301234567',
     email: 'peter@gmail.com',
     registerDate: '2021.11.04.',
-    semester: '2021-2022-1'
+    semester: '2021-2022-1',
+    reason: 'pls',
+    isApprover: false
   },
   {
     name: 'Nagy Péter',
@@ -29,7 +33,22 @@ export class SorompoAdminComponent{
     mobile: '+36301234567',
     email: 'peter@gmail.com',
     registerDate: '2021.11.04.',
-    semester: '2021-2022-1'
+    semester: '2021-2022-1',
+    reason: 'pls',
+    isApprover: false
+  },
+  {
+    name: 'Nagy Péterxd',
+    neptun: 'ABC123',
+    plate: 'CBA321',
+    type: 'Renault Twingo',
+    card: '11111111',
+    mobile: '+36301234567',
+    email: 'peter@gmail.com',
+    registerDate: '2021.11.04.',
+    semester: '2021-2022-1',
+    reason: 'plsplsplsplsplsplsplsplsplsplsplspls plsplsplsplsplsplsplsplsplsplsplspls plsplsplsplsplsplsplspl splsplspls',
+    isApprover: false
   },
   {
     name: 'Nagy Péter',
@@ -40,7 +59,9 @@ export class SorompoAdminComponent{
     mobile: '+36301234567',
     email: 'peter@gmail.com',
     registerDate: '2021.11.04.',
-    semester: '2021-2022-1'
+    semester: '2021-2022-1',
+    reason: 'pls',
+    isApprover: false
   },
   {
     name: 'Nagy Péter',
@@ -51,18 +72,9 @@ export class SorompoAdminComponent{
     mobile: '+36301234567',
     email: 'peter@gmail.com',
     registerDate: '2021.11.04.',
-    semester: '2021-2022-1'
-  },
-  {
-    name: 'Nagy Péter',
-    neptun: 'ABC123',
-    plate: 'CBA321',
-    type: 'Renault Twingo',
-    card: '11111111',
-    mobile: '+36301234567',
-    email: 'peter@gmail.com',
-    registerDate: '2021.11.04.',
-    semester: '2021-2022-1'
+    semester: '2021-2022-1',
+    reason: 'pls',
+    isApprover: false
   },
   {
     name: 'Abdulmuttalib Abdul Kherim',
@@ -73,7 +85,9 @@ export class SorompoAdminComponent{
     mobile: '+36301234567',
     email: 'abdulkherim1998@gmail.com',
     registerDate: '2021.11.04.',
-    semester: '2021-2022-1'
+    semester: '2021-2022-1',
+    reason: 'pls',
+    isApprover: true
   }];
 
 }
