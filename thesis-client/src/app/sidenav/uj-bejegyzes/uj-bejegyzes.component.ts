73cb1779e130cb24c1f854e4fb501b522ac89548
Dalma <cynder097@gmail.com>
2021-11-10 22:21:05 +0100

    Finished styling, was horrible

--- thesis-client/src/app/sidenav/uj-bejegyzes/uj-bejegyzes.component.ts
+++ thesis-client/src/app/sidenav/uj-bejegyzes/uj-bejegyzes.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-uj-bejegyzes',
   templateUrl: './uj-bejegyzes.component.html',
   styleUrls: ['./uj-bejegyzes.component.css']
 })
-export class UjBejegyzesComponent implements OnInit {
+export class UjBejegyzesComponent{
 
-  constructor() { }
+  onSubmit(form: NgForm) {
 
-  ngOnInit(): void {
   }
 
 }
